269bd2392550a177bbd845638f2e7c4f18cd41e1
Geoffrey Wright <Geoff@idpwklt109082.us.mst.net>
2015-06-07 09:21:48 -0400

    updating transformations

--- workers/cat_to_binary/cat_to_binary.py
+++ workers/cat_to_binary/cat_to_binary.py
@@ -28,31 +28,40 @@ def transform2Binary(df):
             df.drop(i,inplace=True,axis=1)
     return df
 
+def aggregate(df):
+    #TODO: write aggregation function
+    #df.pivot_table('')          
+        
+
 big_dict = {}    
     
 #connection to es cluster
 es = Elasticsearch(['http://58d3ea40c46e8b15000.qbox.io:80'])
 
-res = es.search(index="test-index", body={"query": {"match_all": {}}, "size" : 100})
+indexes = ['intervention', 'metadata', 'segment']
 
-for doc in res['hits']['hits']:
-    new_key = doc['_index'] + doc['_type']
-            
-    if doc['_index'] == 'segment':
+for ind in indexes:
 
-        if doc['_type'] == 'identify':
-            #new_doc = handleIdentify(doc)
-            a = 1
+    res = es.search(index=ind, body={"query": {"match_all": {}}, "size" : 100})
+
+    for doc in res['hits']['hits']:
+        new_key = doc['_index'] + '_' + doc['_type']
+                
+        if doc['_index'] == 'segment':
+
+            if doc['_type'] == 'identify':
+                #new_doc = handleIdentify(doc)
+                a = 1
+            else:
+                new_doc = handleTrack(doc)
+        
         else:
-            new_doc = handleTrack(doc)
-    
-    else:
-        new_doc = handleEvent(doc)
-    
-    if new_key in big_dict:
-        big_dict[new_key].append(new_doc)
-    else:
-        big_dict[new_key] = [new_doc]
+            new_doc = handleEvent(doc)
+        
+        if new_key in big_dict:
+            big_dict[new_key].append(new_doc)
+        else:
+            big_dict[new_key] = [new_doc]
 
         
 to_index = {}        
@@ -61,10 +70,18 @@ for data in big_dict.keys():
     events = big_dict[data]
 
     #TODO: confirm this function is doing what is expected
-    df = transform2Binary(DataFrame(events))
+    if data.split('_')[0] == 'metadata':
+        df = transform2Binary(DataFrame(events))
+    elif data.split('_')[0] == 'segment':
+        df = aggregate(DataFrame(events))
+    else:
+        #intervention data
+        #TODO: do something exciting here
     to_index['data'] = df.to_json()
     
 for data in to_index.keys():
     ti = str(time()).split('.')[0]
-    es.index(index="cat-index", doc_type=data, id=ti, body=to_index[data])
+    es.index(index="cat-index", doc_type=data.lower(), id=ti, body=to_index[data])
+
+
 
